9b479bc7f24cf15595c35304c4b79565ef7fcb6b
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-21 00:04:12 +0300

    endpoint to get a list of paginated tours by the travel slug

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelContoller;
 
 Route::prefix('v1')->group(function () {
     Route::get('/travels', [TravelContoller::class, 'index']);
+    Route::get('/travels/{travel:slug}/tours',[TourController::class , 'index']);
 });
 
