0a1247a351c5a946bc75ef2ac2c8a4eb7b67f4ba
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-20 20:38:10 +0300

    endpoint to get a list of paginated public travels

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TravelContoller;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('/travels', [TravelContoller::class, 'index']);
+});
+
