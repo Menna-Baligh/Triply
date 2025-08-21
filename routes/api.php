9801d4f1a39fdac25fb1978e5bb7db4cf4475104
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-22 00:07:00 +0300

    Secured admin travels endpoint  with Sanctum middleware (auth:sanctum)

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelContoller;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 
 Route::prefix('v1')->group(function () {
     Route::get('/travels', [TravelContoller::class, 'index']);
     Route::get('/travels/{travel:slug}/tours',[TourController::class , 'index']);
-    Route::prefix('admin')->group(function (){
+
+    Route::prefix('admin')->middleware('auth:sanctum')->group(function (){
         Route::post('/travels', [App\Http\Controllers\Api\V1\Admin\TravelController::class ,'store']);
     });
+
+    Route::post('/login',LoginController::class);
 });
 
